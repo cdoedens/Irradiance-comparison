1f4a9f2991885e15567e70470c02d4d8db030e9b
cdoedens <carldoedens@gmail.com>
2026-01-28 14:42:31 +1100

    fix rmse and mbe timeseries data and plot annual

---
+++ scripts/031_timeseries_errors.py
@@ -0,0 +1,82 @@
+#!/g/data/xp65/public/apps/med_conda_scripts/analysis3-25.07.d/bin/python3
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+import datetime
+import xesmf as xe
+
+from dask.distributed import Client, wait
+import os, sys
+
+sys.path.append('/home/548/cd3022/repos/Irradiance-comparisons/Irradiance-comparisons')
+import logger
+from read_datasets import read_dataset
+
+LOG = logger.get_logger(__name__)
+
+year = sys.argv[1]
+
+# qsub -I -q normal -P er8 -l walltime=2:00:00,ncpus=24,mem=120GB,jobfs=100MB,storage=gdata/xp65+gdata/er8+gdata/ob53+gdata/rt52+gdata/rv74+gdata/su28
+    
+if __name__ == '__main__':
+    client = Client(
+        n_workers=12,
+        threads_per_worker=1,
+    )
+    LOG.info('dask client started')
+
+    # OPEN DATA
+    him_list = []
+    bar_list = []
+    for month in range(1,13):
+        date = f'{year}-{month:02d}'
+        him = read_dataset(
+                dataset='himawari',
+                resolution='daily',
+                date=date
+            )
+        him_list.append(him)
+        bar = read_dataset(
+                dataset='barra-r2',
+                resolution='daily',
+                date=date
+            )
+        bar_list.append(bar)
+    him = xr.concat(him_list, dim='time')
+    bar = xr.concat(bar_list, dim='time')
+    LOG.info('Datasets opened')
+
+    # PREPROCESS
+    him = him.assign_coords(time=him.time + np.timedelta64(1, "D"))
+    him = him.assign_coords(time=him.time.dt.floor("D"))
+    bar = bar.assign_coords(time=bar.time.dt.floor("D"))
+
+    him = him.interp(
+        lat=bar.lat,
+        lon=bar.lon,
+        method='linear'
+    )
+    LOG.info('Data preprocessed')
+
+    diff = bar.ghi - him.ghi
+
+    rmse = np.sqrt((np.square(diff)).weighted(np.cos(np.deg2rad(diff.lat))).mean(['lat', 'lon']))
+    mbe = diff.weighted(np.cos(np.deg2rad(diff.lat))).mean(['lat', 'lon'])
+    LOG.info('Error metrics calculated')
+
+    ds_errors = xr.Dataset(
+        {
+            'rmse': rmse,
+            'mbe': mbe
+        }
+    )
+
+    ds_errors.attrs['history'] = datetime.date.today().strftime('%D')
+    ds_errors.attrs['source_script'] = 'data produced by the script "031_timeseries_errors.py"'
+    LOG.info('Metadata added')
+
+    file_path = Path(f'/g/data/er8/users/cd3022/Irradiance-comparisons/error_timeseries/')
+    os.makedirs(file_path, exist_ok=True)
+    ds_errors.to_netcdf(f'{file_path}/himawari-barrar2_{year}.nc')
+    LOG.info('Files saved, job complete!')
--- scripts/032_plot_timeseries.py
+++ scripts/032_plot_timeseries.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
 
@@ -6,7 +7,33 @@ file_path = Path('/g/data/er8/users/cd3022/Irradiance-comparisons/error_timeseri
 files = [f for f in file_path.glob('*.nc')]
 ds = xr.open_mfdataset(files)
 
+# remove outliers
+ds['rmse'] = xr.where(ds.rmse > 100, np.nan, ds.rmse)
+ds['mbe'] = xr.where(abs(ds.mbe) > 50, np.nan, ds.mbe)
+
+ds_year = ds.groupby('time.dayofyear').mean('time')
+
+# Plot
+plt.figure(figsize=(16,10))
+plt.plot(ds_year.dayofyear, ds_year.rmse)
+plt.savefig('/home/548/cd3022/figures/Irradiance_comparisons/rmse_timeseries.png')
+plt.close()
+
+plt.figure(figsize=(16,10))
+plt.plot(ds_year.dayofyear, ds_year.mbe)
+plt.savefig('/home/548/cd3022/figures/Irradiance_comparisons/mbe_timeseries.png')
+plt.close()
+
+
+
+
+# Plot
 plt.figure(figsize=(16,10))
 plt.plot(ds.time, ds.rmse)
 plt.savefig('/home/548/cd3022/figures/Irradiance_comparisons/rmse_timeseries.png')
-plt.show()
+plt.close()
+
+plt.figure(figsize=(16,10))
+plt.plot(ds.time, ds.mbe)
+plt.savefig('/home/548/cd3022/figures/Irradiance_comparisons/mbe_timeseries.png')
+plt.close()
