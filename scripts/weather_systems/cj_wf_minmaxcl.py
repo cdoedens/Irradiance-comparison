1dccf545555a44cfab87648e182228205a35d684
cdoedens <carldoedens@gmail.com>
2026-02-06 16:55:12 +1100

    change to monthly loops

--- scripts/weather_systems/cj_wf_minmaxcl.py
+++ scripts/weather_systems/cj_wf_minmaxcl.py
@@ -27,99 +27,101 @@ if __name__ == '__main__':
     LOG.info(f'Starting analysis for dataset {dataset}, weather feature {wf}, year {year}')
 
 
-    # GHI dataset
-    ds_list = []
+    
     for month in range(1, 13):
+        month = f'{month:02d}'
         # LOAD DATASETS
-        ds_month = read_dataset(
+        # GHI dataset
+        ds_ghi = read_dataset(
                 dataset=dataset,
                 resolution='hourly',
-                date=f'{year}-{month:02d}'
+                date=f'{year}-{month}'
             )
+        LOG.info(f'{dataset} data opened')
         ds_list.append(ds_month)
-    ds_ghi = xr.concat(ds_list, dim='time')
-    LOG.info(f'{dataset} data opened')
+    
 
 
-    # Weather feature dataset
-    mask_var_names = {
-        'maxcl': 'FLAG',
-        'mincl': 'INPUT',
-    }
-    mask_var = mask_var_names[wf]
-    def preprocess(_ds):
-        # assign lat/lon values if missing
-        if wf == 'maxcl':
-            lat_vals = np.linspace(-90, 90, 361)
-            lon_vals = np.linspace(-180, 179.75, 720)
-            
-            _ds = _ds.assign_coords(
-                lat=lat_vals,
-                lon=lon_vals
+        # Weather feature dataset
+        mask_var_names = {
+            'maxcl': 'FLAG',
+            'mincl': 'INPUT',
+        }
+        mask_var = mask_var_names[wf]
+        def preprocess(_ds):
+            # assign lat/lon values if missing
+            if wf == 'maxcl':
+                lat_vals = np.linspace(-90, 90, 361)
+                lon_vals = np.linspace(-180, 179.75, 720)
+                
+                _ds = _ds.assign_coords(
+                    lat=lat_vals,
+                    lon=lon_vals
+                )
+            return _ds.sel(
+                lat=slice(ds_ghi.lat.min(), ds_ghi.lat.max()),
+                lon=slice(ds_ghi.lon.min(), ds_ghi.lon.max())
             )
-        return _ds.sel(
-            lat=slice(ds_ghi.lat.min(), ds_ghi.lat.max()),
-            lon=slice(ds_ghi.lon.min(), ds_ghi.lon.max())
+        wf_path = Path(f'/g/data/su28/weatherfeatures.era5/{wf}/')
+        wf_files = [f for f in wf_path.rglob(f'*{year}_{month}*.nc')]
+        ds_wf = xr.open_mfdataset(wf_files) #, preprocess=preprocess)
+        if wf == 'mincl':
+            ds_wf = ds_wf.isel(dimz_INPUT=0)
+
+        # PREPROCESS DATASETS BEFORE MASKING
+        # Interpolate grid cells
+        ds_wf = ds_wf.interp(
+            lat=ds_ghi.lat,
+            lon=ds_ghi.lon,
+            method='nearest'
         )
-    wf_path = Path(f'/g/data/su28/weatherfeatures.era5/{wf}/')
-    wf_files = [f for f in wf_path.rglob(f'*{year}*.nc')]
-    ds_wf = xr.open_mfdataset(wf_files) #, preprocess=preprocess)
-    if wf == 'mincl':
-        ds_wf = ds_wf.isel(dimz_INPUT=0)
 
-    # PREPROCESS DATASETS BEFORE MASKING
-    # Interpolate grid cells
-    ds_wf = ds_wf.interp(
-        lat=ds_ghi.lat,
-        lon=ds_ghi.lon,
-        method='nearest'
-    )
+        # Extra Himawari preprocessing steps
+        if dataset == 'himawari':
+            start = ds_ghi.time.min().item()
+            end = ds_ghi.time.max().item()
+            # fill missing overnight time steps
+            full_time = pd.date_range(
+                start=start,
+                end=end,
+                freq="60min"
+            )
+            ds_ghi = ds_ghi.reindex(time=full_time, fill_value=0)
+            
+            # remove timesteps from the end of wf_ds to match himawari,
+            # missing timesteps are overnight so will not impact final data
+            ds_wf = ds_wf.isel(time=slice(0, -14)
 
-    # Extra Himawari preprocessing steps
-    if dataset == 'himawari':
-        start = ds_ghi.time.min().item()
-        end = ds_ghi.time.max().item()
-        # fill missing overnight time steps
-        full_time = pd.date_range(
-            start=start,
-            end=end,
-            freq="60min"
-        )
-        ds_ghi = ds_ghi.reindex(time=full_time, fill_value=0)
-        
-        # remove timesteps from the end of wf_ds to match himawari,
-        # missing timesteps are overnight so will not impact final data
-        ds_wf = ds_wf.sel(time=slice(f'{year}-01-01T00', f'{year}-12-31T10:30'))
+        # Align times
+        ds_ghi_shifted = ds_ghi.assign_coords(time=ds_ghi.time - pd.Timedelta('30min'))
+        ds_ghi_times = ds_ghi_shifted.sel(time=ds_wf.time)
 
-    # Align times
-    ds_ghi_shifted = ds_ghi.assign_coords(time=ds_ghi.time - pd.Timedelta('30min'))
-    ds_ghi_times = ds_ghi_shifted.sel(time=ds_wf.time)
+        LOG.info('preprocessing complete')
 
-    LOG.info('preprocessing complete')
+        # APPLY MASK
+        mask = ds_wf[mask_var]
+        masked_ghi = xr.where(mask != 0, ds_ghi_times.ghi, np.nan)
 
-    # APPLY MASK
-    mask = ds_wf[mask_var]
-    masked_ghi = xr.where(mask != 0, ds_ghi_times.ghi, np.nan)
+        # TIME MEAN
+        masked_ghi_mean = masked_ghi.mean(dim='time')
 
-    # TIME MEAN
-    masked_ghi_mean = masked_ghi.mean(dim='time')
-
-    # remask himawari region
-    if dataset == 'himawari':
-        masked_ghi_mean = xr.where(ds_ghi.isel(time=5).ghi.isnull(), np.nan, masked_ghi_mean)
+        # remask himawari region
+        if dataset == 'himawari':
+            masked_ghi_mean = xr.where(ds_ghi.isel(time=5).ghi.isnull(), np.nan, masked_ghi_mean)
 
 
-    # add metadata
-    masked_ghi_mean = masked_ghi_mean.to_dataset(name='annual_mean_ghi')
-    masked_ghi_mean = masked_ghi_mean.assign_coords({'year':year})
-    masked_ghi_mean.attrs['date_generated'] = datetime.date.today().strftime('%D')
-    masked_ghi_mean.attrs['source_script'] = 'data produced by the script "cj_wf_minmaxcl.py"'
+        # add metadata
+        masked_ghi_mean = masked_ghi_mean.to_dataset(name='monthly_mean_ghi')
+        masked_ghi_mean = masked_ghi_mean.assign_coords({'date':f'{year}-{month}')
+        masked_ghi_mean.attrs['date_generated'] = datetime.date.today().strftime('%D')
+        masked_ghi_mean.attrs['source_script'] = 'data produced by the script "cj_wf_minmaxcl.py"'
 
-    # SAVE DATA
-    file_path = Path(f'/g/data/er8/users/cd3022/Irradiance-comparisons/weather-features-cj/{wf}')
-    os.makedirs(file_path, exist_ok=True)
-    masked_ghi_mean.to_netcdf(f'{file_path}/{dataset}-{year}.nc')
-    LOG.info('data saved, job complete!')
+        # SAVE DATA
+        file_path = Path(f'/g/data/er8/users/cd3022/Irradiance-comparisons/weather-features-cj/{wf}')
+        os.makedirs(file_path, exist_ok=True)
+        masked_ghi_mean.to_netcdf(f'{file_path}/{dataset}-{year}{month}.nc')
+        LOG.info('Month data saved')
+    LOG.info('Job complete!')
 
     
 
--- scripts/weather_systems/sea_breezes.py
+++ scripts/weather_systems/sea_breezes.py
@@ -23,94 +23,92 @@ if __name__ == '__main__':
     year = sys.argv[2]
     LOG.info(f'Starting analysis for dataset {dataset}, sea breezes, year {year}')
 
-    # GHI dataset
-    ds_list = []
     for month in range(1, 13):
+        month = f'{month:02d}'
         # LOAD DATASETS
-        ds_month = read_dataset(
+        ds = read_dataset(
                 dataset=dataset,
                 resolution='hourly',
-                date=f'{year}-{month:02d}'
+                date=f'{year}-{month}'
             )
-        ds_list.append(ds_month)
-    ds = xr.concat(ds_list, dim='time')
-    LOG.info(f'{dataset} data opened')
+        LOG.info(f'{dataset} data opened')
 
-    sb_path = Path('/g/data/ng72/ab4502/sea_breeze_detection/barra_c_smooth_s2/filters/')
-    sb_files = [f for f in sb_path.glob(f'*_F_{year}*.zarr')]
-    ds_sb = xr.open_mfdataset(
-        sb_files,
-        engine="zarr",
-        combine="by_coords",
-        compat='override',
-        coords='minimal',
-        parallel=True,
-    )
-    LOG.info('sea breeze data opened')
-
-    # PREPROCESS DATASETS TO MATCH TIMES/SHAPES
-    # interp wf mask from era5 to barra
-    ds_sb = ds_sb.interp(
-        lat=ds.lat,
-        lon=ds.lon,
-        method='nearest'
-    )
-    
-    # adjust hourly times on the 30min to match 3hr times (on the hour) from wf
-    ds_shifted = ds.assign_coords(time=ds.time + pd.Timedelta('30min'))
-    
-    if dataset == 'himawari':
-        # fill missing overnight time steps
-        full_time = pd.date_range(
-            start=ds_shifted.time.min().item(),
-            end=ds_shifted.time.max().item(),
-            freq="60min"
+        sb_path = Path('/g/data/ng72/ab4502/sea_breeze_detection/barra_c_smooth_s2/filters/')
+        sb_files = [f for f in sb_path.glob(f'*_F_{year}{month}*.zarr')]
+        ds_sb = xr.open_mfdataset(
+            sb_files,
+            engine="zarr",
+            combine="by_coords",
+            compat='override',
+            coords='minimal',
+            parallel=True,
         )
-        ds_shifted = ds_shifted.reindex(time=full_time, fill_value=0)
+        LOG.info('sea breeze data opened')
+
+        # PREPROCESS DATASETS TO MATCH TIMES/SHAPES
+        # interp wf mask from era5 to barra
+        ds_sb = ds_sb.interp(
+            lat=ds.lat,
+            lon=ds.lon,
+            method='nearest'
+        )
         
-        # line up datasets
-        min_time = ds_sb.time.min()
-        max_time = ds_shifted.time.max()
-        t_range = slice(min_time, max_time)
-        ds_sb = ds_sb.sel(time=t_range)
-        ds = ds.sel(time=t_range)
+        # adjust hourly times on the 30min to match 3hr times (on the hour) from wf
+        ds_shifted = ds.assign_coords(time=ds.time + pd.Timedelta('30min'))
+        
+        if dataset == 'himawari':
+            # fill missing overnight time steps
+            full_time = pd.date_range(
+                start=ds_shifted.time.min().item(),
+                end=ds_shifted.time.max().item(),
+                freq="60min"
+            )
+            ds_shifted = ds_shifted.reindex(time=full_time, fill_value=0)
+            
+            # line up datasets
+            min_time = ds_sb.time.min()
+            max_time = ds_shifted.time.max()
+            t_range = slice(min_time, max_time)
+            ds_sb = ds_sb.sel(time=t_range)
+            ds = ds.sel(time=t_range)
 
-    ds_times = ds_shifted.sel(time=ds_sb.time)
-    LOG.info('preprocessing complete')
+        ds_times = ds_shifted.sel(time=ds_sb.time)
+        LOG.info('preprocessing complete')
 
-    # Extend mask to capture larger area round sea breeze
-    mask = ds_sb.mask.fillna(False).astype(bool)
-    sb_extension = 2 # hours
-    sb_extended = (
-        mask
-        | mask.shift(time=sb_extension).fillna(False)
-        | mask.shift(time=-sb_extension).fillna(False)
-    )
-    LOG.info(f'sb mask extended forward and back {sb_extension} hours')
-    
+        # Extend mask to capture larger area round sea breeze
+        mask = ds_sb.mask.fillna(False).astype(bool)
+        sb_extension = 2 # hours
+        sb_extended = (
+            mask
+            | mask.shift(time=sb_extension).fillna(False)
+            | mask.shift(time=-sb_extension).fillna(False)
+        )
+        LOG.info(f'sb mask extended forward and back {sb_extension} hours')
+        
         # APPLY MASK
-    sb_ghi = xr.where(sb_extended != 0, ds_times.ghi, np.nan)
-    LOG.info('mask applied')
+        sb_ghi = xr.where(sb_extended != 0, ds_times.ghi, np.nan)
+        LOG.info('mask applied')
 
-    # TIME MEAN
-    sb_ghi_mean = sb_ghi.mean(dim='time')
-    LOG.info('annual mean calculated')
+        # TIME MEAN
+        sb_ghi_mean = sb_ghi.mean(dim='time')
+        LOG.info('annual mean calculated')
 
-    # remask himawari region
-    if dataset == 'himawari':
-        sb_ghi_mean = xr.where(ds.isel(time=5).ghi.isnull(), np.nan, sb_ghi_mean)
+        # remask himawari region
+        if dataset == 'himawari':
+            sb_ghi_mean = xr.where(ds.isel(time=5).ghi.isnull(), np.nan, sb_ghi_mean)
 
-    # add metadata
-    sb_ghi_mean = sb_ghi_mean.to_dataset(name='annual_mean_ghi')
-    sb_ghi_mean = sb_ghi_mean.assign_coords({'year':year})
-    sb_ghi_mean.attrs['date_generated'] = datetime.date.today().strftime('%D')
-    sb_ghi_mean.attrs['source_script'] = 'data produced by the script "041.5_sea_breezes.py"'
+        # add metadata
+        sb_ghi_mean = sb_ghi_mean.to_dataset(name='annual_mean_ghi')
+        sb_ghi_mean = sb_ghi_mean.assign_coords({'year':year})
+        sb_ghi_mean.attrs['date_generated'] = datetime.date.today().strftime('%D')
+        sb_ghi_mean.attrs['source_script'] = 'data produced by the script "sea_breezes.py"'
 
-    # SAVE DATA
-    file_path = Path(f'/g/data/er8/users/cd3022/Irradiance-comparisons/weather-features/sea_breaze')
-    os.makedirs(file_path, exist_ok=True)
-    sb_ghi_mean.to_netcdf(f'{file_path}/{dataset}-{year}.nc')
-    LOG.info('data saved, job complete!')
+        # SAVE DATA
+        file_path = Path(f'/g/data/er8/users/cd3022/Irradiance-comparisons/weather-features/sea_breaze')
+        os.makedirs(file_path, exist_ok=True)
+        sb_ghi_mean.to_netcdf(f'{file_path}/{dataset}-{year}-{month}.nc')
+        LOG.info('Monthly data saved')
+    LOG.info('Job complete!')
 
 
 
