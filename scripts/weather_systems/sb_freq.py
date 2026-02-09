6dd34f2c54bab74a894247ff406727fb421f2311
cdoedens <carldoedens@gmail.com>
2026-02-09 21:34:59 +1100

    success plotting SBgit add scripts/weather_systems/sb_freq.py ! (issues with freq in 2016?)

--- scripts/weather_systems/sb_freq.py
+++ scripts/weather_systems/sb_freq.py
@@ -19,38 +19,29 @@ from read_datasets import read_dataset
 GET THE SPATIAL FREQ OF SEA BREEZE OBJECTS
 '''
 
-if __name__ == '__main__':
+LOG = logger.get_logger(__name__)
 
-    client = Client(
-        n_workers=48,
-        threads_per_worker=1
-    )
+year = sys.argv[1]
 
-    sb_years = []
-    for year in range(2016, 2025):
-        sb_path = Path('/g/data/ng72/ab4502/sea_breeze_detection/barra_c_smooth_s2/filters/')
-        sb_files = [f for f in sb_path.glob(f'*_F_{year}*.zarr')]
-        ds_sb = xr.open_mfdataset(
-            sb_files,
-            engine="zarr",
-            combine="by_coords",
-            compat='override',
-            coords='minimal',
-            parallel=True,
-        )
+month_list = []
+freq = 0
+for month in range(1, 13):
+    month=f'{month:02d}'
+    sb_path = Path('/g/data/ng72/ab4502/sea_breeze_detection/barra_c_smooth_s2/filters/')
+    sb_files = [f for f in sb_path.glob(f'*_F_{year}{month}*.zarr')][0]
+    ds_sb = xr.open_zarr(sb_files)
+    
+    mask = ds_sb.mask
+    sb_extended = mask.rolling(time = 5, center=True).max()
+    LOG.info('Mask applied and extended')
+    
+    counts = xr.where(sb_extended != 0, 1, 0).sum(dim='time')
+    counts = counts.assign_coords(month=month)
+    freq += ds_sb.sizes['time']
+    month_list.append(counts)
+all_mon = xr.concat(month_list, dim='month')
+year_freq = all_mon.sum(dim='month')
+year_freq = year_freq.assign_coords(year=year, freq=freq)
 
-        mask = ds_sb.mask.fillna(False).astype(bool)
-        sb_extension = 2 # hours
-        sb_extended = (
-            mask
-            | mask.shift(time=sb_extension).fillna(False)
-            | mask.shift(time=-sb_extension).fillna(False)
-        )
-
-        counts = xr.where(sb_extended != 0, 1, 0).sum(dim='time')
-        counts = counts.assign_coords(year=year)
-        sb_years.append(counts)
-    all_counts = xr.concat(sb_years, dim='year')
-    total_freq = all_counts.sum(dim='year')
-    total_freq.to_netcdf('/g/data/er8/users/cd3022/Irradiance-comparisons/weather-features/sea_breeze/TOTAL_FREQ.nc')
-        
+year_freq.to_netcdf(f'/g/data/er8/users/cd3022/Irradiance-comparisons/weather-features/sea_breeze/TOTAL_FREQ_{year}.nc')
+LOG.info('Job complete!')
